8745bd908485fd22de5ebcbbf1b9c1a1dd669b74
Mihail Arhipov <arhipov_ma@mail.ru>
2020-02-17 15:08:09 +0300

    connected to db. BUG: not always u can c post on da home pagggge

--- app.js
+++ app.js
@@ -13,24 +13,49 @@ app.set('view engine', 'ejs'); //используем ejs
 app.use(bodyParser.urlencoded({extended: true})); //используем body-parser
 app.use(express.static("public")); //статичные файлы (стили) располагаем в папке public, чтобы сервер мог прочитать их не только на локальном (нашем) устройстве
 
+//Подключаемся к БД
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
+//Статичный стартовый контент страниц
 const homeContent = "Posts'd be stored there";
 const aboutContent = "About me";
 const contactContent = "Contact me";
 
-let posts = []
+//Шаблон, по которому строится пост
+const postSchema = new mongoose.Schema ({
+    title: {
+        type: String,
+        required: [true, "this field needs to be filled"],
+    },
+    post: {
+        type: String,
+        required: [true, "this field needs to be filled"],
+    }
+});
+const Post = mongoose.model("Post", postSchema);
+
+
+const postsListSchema = new mongoose.Schema({
+    name: String,
+    posts: [postSchema]
+})
+const postsList = mongoose.model("postsList", postsListSchema);
 
 
 
 app.get("/", function(req, res){
-    postsContentVisible = []
-    
-    res.render("home", {
-        homeContent: homeContent,
-        posts: posts
-        // posts: postsContentVisible
+
+    Post.find({}, function(err, foundPosts) {
+        res.render("home", {
+            homeContent: homeContent,
+            posts: foundPosts
+        });
     });
-})
+});
+
+
+
+
 app.get("/about", function(req, res){
     res.render("about", {aboutContent: aboutContent});
 })
@@ -40,12 +65,16 @@ app.get("/contact", function(req, res){
 app.get("/compose", function(req, res){
     res.render("compose");
 })
+
+
 app.post("/compose", function (req, res) {
-    const input = {
+    
+    const post = new Post({
         title: req.body.title,
         post: req.body.post
-    }
-    posts.push(input);
+    });
+    console.log(post);
+    post.save();
     // res.redirect("/", {newTitle: input.title, newPost: input.post});
     res.redirect("/")
 })
